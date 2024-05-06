5dc31f27e4efaa5f6237e22c1ff80973fd957aca
lsh <linshshr@126.com>
2024-05-06 15:37:25 +0800

    fix pom.xml

--- src/main/java/com/example/online_java_ide/utils/mainExecutor.java
+++ src/main/java/com/example/online_java_ide/utils/mainExecutor.java
@@ -10,9 +10,9 @@ public class mainExecutor {
     public static String execute(byte[] classByte, String systemIn) {
         ClassModifier classModifier = new ClassModifier(classByte);
 
-        byte[] modifyBytes = classModifier.modifyConstantUTF8Value("java/lang/System", "com/example/online_java_ide/utils/CustomSystem");
+        byte[] modifyBytes = classModifier.modifyConstantUTF8Value("java/lang/System", "BOOT-INF/classes/com/example/online_java_ide/utils/CustomSystem");
 //        System.out.println(System.getProperty("java.lang.System"));
-        modifyBytes = classModifier.modifyConstantUTF8Value("java/util/Scanner", "com/example/online_java_ide/utils/CustomScanner");
+        modifyBytes = classModifier.modifyConstantUTF8Value("java/util/Scanner", "BOOT-INF/classes/com/example/online_java_ide/utils/CustomScanner");
 
         ((CustomInputStream) CustomSystem.in).set(systemIn);
 
